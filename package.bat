dd8bb4a71134ce06e55b97e6cf87ee1780e47c1f
zou.mono <zou.mono@gmail.com>
2024-02-01 21:48:56 +0800

    修复打包文件,测试通过.

--- package.bat
+++ package.bat
@@ -4,7 +4,9 @@ cd ..
 
 for %%i in ("%CD%") do set curfolder=%%~fsi
 
-call python-qgis-ltr -m pyarmor.cli gen -O dist -r -i renderup --exclude renderup/__init__.py --exclude renderup/resources_rc.py --exclude renderup/__pycache__
+rmdir /q /s "%curfolder%\dist"
+
+call python-qgis-ltr -m pyarmor.cli gen --platform windows.x86_64 --platform darwin.x86_64 -O dist -r -i renderup --exclude renderup/__init__.py --exclude renderup/resources_rc.py --exclude renderup/__pycache__
 
 REM copy addition files
 echo %curfolder%
@@ -12,6 +14,7 @@ echo %~dp0
 call copy "%~dp0\metadata.txt" "%curfolder%\dist\renderup\metadata.txt"
 call copy "%~dp0\extramaps.yml" "%curfolder%\dist\renderup\extramaps.yml"
 call copy "%~dp0\resources_rc.py" "%curfolder%\dist\renderup\resources_rc.py"
+call copy "%~dp0\__init__.py" "%curfolder%\dist\renderup\__init__.py"
 
 mkdir "%curfolder%\dist\renderup\icons\"
 call copy "%~dp0\icons\icon.png" "%curfolder%\dist\renderup\icons\icon.png"
