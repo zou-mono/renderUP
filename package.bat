4e43278121d3f2ee50ba120a1482636e42a43a2b
zou.mono <zou.mono@gmail.com>
2024-02-01 21:37:25 +0800

    制作打包文件.

--- package.bat
+++ package.bat
@@ -4,7 +4,7 @@ cd ..
 
 for %%i in ("%CD%") do set curfolder=%%~fsi
 
-call python-qgis-ltr -m pyarmor.cli gen -O dist -r -i renderup --exclude renderup/resources_rc.py --exclude renderup/__pycache__
+call python-qgis-ltr -m pyarmor.cli gen -O dist -r -i renderup --exclude renderup/__init__.py --exclude renderup/resources_rc.py --exclude renderup/__pycache__
 
 REM copy addition files
 echo %curfolder%
@@ -12,6 +12,13 @@ echo %~dp0
 call copy "%~dp0\metadata.txt" "%curfolder%\dist\renderup\metadata.txt"
 call copy "%~dp0\extramaps.yml" "%curfolder%\dist\renderup\extramaps.yml"
 call copy "%~dp0\resources_rc.py" "%curfolder%\dist\renderup\resources_rc.py"
+
+mkdir "%curfolder%\dist\renderup\icons\"
+call copy "%~dp0\icons\icon.png" "%curfolder%\dist\renderup\icons\icon.png"
+call copy "%~dp0\icons\metro_station.svg" "%curfolder%\dist\renderup\icons\metro_station.svg"
+
+7z a -r "%curfolder%\dist\renderup.zip" "%curfolder%\dist\renderup\"
+
 echo "package success."
 
 popd
